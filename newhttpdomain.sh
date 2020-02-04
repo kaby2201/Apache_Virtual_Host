10d9cb4170e971c2c492a73ba4978800d92e7f15
Byamungu Kabiraba <byamungukabiraba@gmail.com>
2020-02-04 12:51:36 +0100

    Create newhttpdomain.sh

---
+++ newhttpdomain.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Create soulder
+echo "How To Set Up Apache Virtual Hosts for Ubuntu 16.04"
+echo "Enter domain name (byamungu.com or websitename.x):"
+read DOMAIN_NAME
+echo "Enter email address for webadmin (this will show app in std.404-page)"
+read DOMAIN_ADMIN_EMAIL
+echo "Do you want to disable the default page? (Y/N)"
+read DISABLE_SITE
+
+sudo mkdir -p /var/www/$DOMAIN_NAME/public_html
+sudo chown -R $USER:$USER /var/www/$DOMAIN_NAME/public_html
+sudo chmod -R 755 /var/www
+sudo tee -a "/var/www/$DOMAIN_NAME/public_html/index.html" > /dev/null <<EOT
+<html>
+  <head>
+    <title>Welcome to $DOMAIN_NAME!</title>
+  </head>
+  <body> <h1>Success!  The $DOMAIN_NAME virtual host is working!</h1>
+  <p>Please consider making a small <a href="https://commerce.coinbase.com/checkout/b005ae5c-0509-491a-a84b-756057e43a0d">donation</a></p>
+  </body>
+</html>
+EOT
+sudo tee -a "/etc/apache2/sites-available/$DOMAIN_NAME.conf" > /dev/null <<EOT
+<VirtualHost *:80>
+        ServerAdmin $DOMAIN_ADMIN_EMAIL
+        ServerName $DOMAIN_NAME
+        ServerAlias www.$DOMAIN_NAME
+        DocumentRoot /var/www/$DOMAIN_NAME/public_html
+
+        <Directory /var/www/html/>
+            Options Indexes FollowSymLinks
+            AllowOverride All
+            Require all granted
+        </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOT
+
+sudo a2ensite $DOMAIN_NAME.conf
+if $DISABLE_SITE -eq "Y"
+then
+  sudo a2dissite 000-default.conf
+fi
+sudo service apache2 restart
+sudo systemctl restart apache2
+echo "The web site $DOMAIN_NAME is now online!"
